91de897a59e127b4cfabc6c912985018c04605a1
nms377 <nms377@nyu.edu>
2016-11-12 14:40:28 -1000

    The actual results were way simpler than my inital work.

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
+
+var secretNumber = Math.random()*1000000;		//declare a var that wil produce your random number. This is in the parent scope
+
+
+	return function(){			//this invokes the method in your var b/c a var cannot invoke it for you
+		return secretNumber;	// this returns the result of the method in your global var
+	};
+
 };
+
+// number range 0 to 1000000
+
+// call a random number
+
+	//	Use Math.random();
+
+// call same random number every time function is invoked
+
